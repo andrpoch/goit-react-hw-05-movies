fa91e4c77b1e371bb716991d57381d4bf5f9f555
andrpoch <andrpoch25@gmail.com>
2022-04-25 23:09:18 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom';
-import  App  from 'components/App';
+import App from 'components/App';
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter basename='/goit-react-hw-05-movies/'>
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.querySelector("#root")
